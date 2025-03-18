d7d1ba18a9674b935f7deb889fef5a53ce853fa7
mlobeto <mercedeslobeto@gmail.com>
2025-03-18 16:57:54 -0300

    toast

--- FrontBalu/src/utils/toastMiddleware.js
+++ FrontBalu/src/utils/toastMiddleware.js
@@ -1,16 +1,27 @@
 import { toast } from 'react-toastify';
 
-// Este middleware detecta acciones que terminen en "SUCCESS" o "FAILURE"
-// y lanza un toast con el mensaje, si existe.
+const toastConfig = {
+  position: "top-center", // Ubicación del toast
+  autoClose: 3000, // Duración en ms
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "dark", // Tema del toast (light, dark, colored)
+};
+
 const toastMiddleware = () => (next) => (action) => {
   // Si la acción es de éxito y tiene 'message' en payload, mostrar toast de éxito.
   if (action.type.endsWith('SUCCESS') && action.payload?.message) {
-    toast.success(action.payload.message);
+    toast.success(action.payload.message, toastConfig);
   }
+  
   // Si la acción es de fallo y tiene mensaje en payload, mostrar toast de error.
   if (action.type.endsWith('FAILURE') && action.payload) {
-    toast.error(action.payload);
+    toast.error(action.payload, toastConfig);
   }
+  
   return next(action);
 };
 
