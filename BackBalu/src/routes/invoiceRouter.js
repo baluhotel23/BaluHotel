50b48a9dcb6fb7a9295673a95227ae29c3e1b36b
mlobeto <mercedeslobeto@gmail.com>
2025-05-23 13:43:02 -0300

    wompi funcionando

--- BackBalu/src/routes/invoiceRouter.js
+++ BackBalu/src/routes/invoiceRouter.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const controller  = require('../controller/invoiceControllers');
+const controller = require('../controller/invoiceControllers');
+//const taxxaController = require('../controller/Taxxa/TaxxaService');
 
+// Ruta para obtener todas las facturas
+router.get('/all', controller.getAllInvoices);
 
-router.post('/', controller.postInvoice);
+// Ruta para obtener el último número de factura
+router.get('/lastNumber', controller.getLastInvoiceNumber);
+router.get('/allInvoices', controller.getAllInvoices);
+// Rutas dinámicas al final
 router.get('/:status', controller.getInvoicesByStatus);
+//router.post('/', controller.postInvoice);
 
 module.exports = router;
