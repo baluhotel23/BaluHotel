584b16ca78cb5f13d38862a48d89358f8e858eb8
mlobeto <mercedeslobeto@gmail.com>
2025-06-02 22:40:59 -0300

    axios

--- FrontBalu/src/utils/axios.js
+++ FrontBalu/src/utils/axios.js
@@ -4,7 +4,7 @@ import { logout } from '../Redux/Reducer/authReducer';
 
 // Crear instancia de axios con la URL base
 const api = axios.create({
-  baseURL: 'http://localhost:3001/', // Ajusta esto a tu URL de backend
+  baseURL: 'https://baluhotel.onrender.com/', // Ajusta esto a tu URL de backend
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
